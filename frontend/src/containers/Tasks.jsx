330c6943467ebd02a040d6cad1a786b9dc53dc29
to-Kenji <kura101089kenji@gmail.com>
2021-02-22 12:17:00 +0900

    tasks#indexのレイアウト
    close #4

--- frontend/src/containers/Tasks.jsx
+++ frontend/src/containers/Tasks.jsx
@@ -1,7 +1,21 @@
 import React, { Fragment, useEffect, useReducer } from "react";
+
 import { fetchTasks } from "../apis/fetchTasks";
 import { fetchTasksReducer, initialState, tasksActionTypes } from "../reducers/fetchTasksReducer";
 
+import styled from 'styled-components';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+
+const TasksPaper = styled(Paper)`
+  margin-top: 20px;
+  margin-bottom: 20px;
+  height: 40px;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+`;
 
 export const Tasks = () => {
   const [tasksState, dispatch] = useReducer(fetchTasksReducer, initialState);
@@ -20,12 +34,22 @@ export const Tasks = () => {
   }, [tasksState.tasksList.length])
 
   return (
-    <div>
-      {
-        tasksState.tasksList.map((task, index) => 
-          <p key={index}>{task.title}</p>
-        )
-      }
-    </div>
+    <Fragment>
+      <Grid container justify="space-around">
+        <Grid item xs={8}>
+          {
+            tasksState.tasksList.map((task, index) => 
+              <TasksPaper key={index}>      
+                  <Grid item xs={10}>
+                    <p>
+                      {task.title}
+                    </p>
+                  </Grid>
+              </TasksPaper>
+            )
+          } 
+        </Grid>
+      </Grid>
+    </Fragment>
   )
 };
