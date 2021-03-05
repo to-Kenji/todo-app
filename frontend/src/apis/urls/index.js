332b469522e371ecb09bdc1fc3c3257e542feacb
to-Kenji <kura101089kenji@gmail.com>
2021-03-05 17:53:33 +0900

    本番環境への準備

--- frontend/src/apis/urls/index.js
+++ frontend/src/apis/urls/index.js
@@ -1,4 +1,4 @@
-const API_SERVER = 'http://localhost:3001/api/v1';
+const API_SERVER = process.env.REACT_APP_API_SERVER
 
 export const tasksIndexUrl = (userId) => `${API_SERVER}/users/${userId}/tasks`;
 export const taskUrl = (params) => `${API_SERVER}/users/${params.userId}/tasks/${params.taskId}`;
